9c7cc69dc453a072aa6f359794e6118e8fe9c6e0
eSwAr9798 <EMekarao01@indianatech.net>
2024-11-14 21:09:36 -0500

    Add new VS Code files

--- capstone-wallet-mangment -by eswar/src/Header.jsx
+++ capstone-wallet-mangment -by eswar/src/Header.jsx
@@ -1,18 +1,19 @@
+
 function Header() {
  
   return (
     <header>
-           <h1>CAPSTONE-PROJECT</h1>
-            <nav>
-              <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">MARKET-INSYTE</a></li>
-                <li><a href="#">MARKET-OVERVIEW</a></li>
-                <li><a href="#">CONTACT</a></li>
-             </ul>
-            </nav>
-           <hr />
-       </header>
-    );
-     } 
+      <h1>CAPSTONE-PROJECT</h1>
+      <nav>
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">MARKET-INSYTE</a></li>
+          <li><a href="#">MARKET-OVERVIEW</a></li>
+          <li><a href="#">CONTACT</a></li>
+        </ul>
+      </nav>
+      <hr />
+    </header>
+  );
+}
 export default Header
